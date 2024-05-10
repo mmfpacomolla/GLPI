bf0e20d41d28806874fdeb220f22af802c138822
root <root@GLPi.192.168.0.1>
2024-05-10 11:37:21 +0200

    First commit

---
+++ config/config_db.php
@@ -0,0 +1,12 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = 'localhost';
+   public $dbuser = 'root';
+   public $dbpassword = 'portalet%4078';
+   public $dbdefault = 'glpi';
+   public $allow_datetime = false;
+   public $use_utf8mb4 = true;
+   public $allow_myisam = false;
+   public $allow_signed_keys = false;
+   public $use_timezones = true;
+}
---
+++ marketplace/genericobject/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '2.14.9',
+        'version' => '2.14.9.0',
+        'reference' => '36fe87f39158056bef9612d10b96c8dc745ee157',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '2.14.9',
+            'version' => '2.14.9.0',
+            'reference' => '36fe87f39158056bef9612d10b96c8dc745ee157',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
